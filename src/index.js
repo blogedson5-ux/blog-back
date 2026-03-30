207ac85d20b129b22eec9c63f4d772bfb55ab81e
blogedson5-ux <blogedson5@gmail.com>
2026-03-29 23:55:01 -0300

    Atualizacao de SEO

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://perfiledsonferreira.netlify.app"],
+    origin: ["http://localhost:3000", "https://perfiledsonferreira.vercel.app"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   }),
